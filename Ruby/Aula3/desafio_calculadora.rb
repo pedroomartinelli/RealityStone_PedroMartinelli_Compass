76d2de942aa2ce70c117ee38e5c072821a5142af
Pedro Martinelli <pedro.martinelli.pb@compasso.com.br>
2023-08-11 16:18:31 -0300

    Desafio aula 3

---
+++ Ruby/Aula3/desafio_calculadora.rb
@@ -0,0 +1,45 @@
+
+loop do
+    print "Digite o primeiro número: "
+    number1 = gets.chomp.to_f
+    print "Digite o segundo número: "
+    number2 = gets.chomp.to_f
+
+    puts "# SELECIONE A OPERAÇÃO #"
+    puts "1 - ADIÇÃO (+)"
+    puts "2 - SUBTRAÇÃO (-)"
+    puts "3 - MULTIPLICAÇÃO (*)"
+    puts "4 - DIVISÃO (/)"
+    
+
+    print "Opção: "
+    option = gets.chomp.to_i
+
+    case option
+    when 1
+        result = number1 + number2
+    when 2
+        result = number1 - number2
+    when 3
+        result = number1 * number2
+    when 4
+        result = number1 / number2
+    else
+        puts "Opção inválida!"
+    end
+
+    puts result
+    
+    puts "Deseja continuar na calculadora?"
+    puts "1 - SIM :)"
+    puts "0 - NÃO :("
+
+    leave = gets.chomp.to_i
+
+    if leave == 0
+        break
+    end
+end
+
+
+    
