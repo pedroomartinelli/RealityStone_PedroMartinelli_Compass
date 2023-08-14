e017566e9c4736c8b2c9de2937f8f21c6cff1a2d
Pedro Martinelli <pedro.martinelli.pb@compasso.com.br>
2023-08-14 15:21:47 -0300

    Aula 5

---
+++ Ruby/Aula5/method.rb
@@ -0,0 +1,10 @@
+def talk(first_name, last_name)
+    puts "Olá #{first_name} #{last_name} como você está?"
+end
+
+first_name = 'Pedro'
+last_name = 'Vieira Martinelli'
+
+talk(first_name, last_name)
+talk('Pedro', 'Vieira Martinelli')
+
